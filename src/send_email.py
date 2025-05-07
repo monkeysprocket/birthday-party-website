b4c33c4eea7a161bab923781ff646d6d46c5f642
Matt Quinn <mjquinn88@outlook.com>
2025-05-07 17:50:02 +0100

    refactored send_email.py to use boto3

--- src/send_email.py
+++ src/send_email.py
@@ -1,20 +1,39 @@
-import smtplib
 import os
-from email.mime.text import MIMEText
-from email.utils import formataddr
+
+import boto3
+from botocore.exceptions import ClientError
+
+ses = boto3.client("ses", region_name=os.environ["AWS_REGION"])
 
 
-def send_invite_email(name, email, invite_id):
+def send_invite_email(name: str, email: str, invite_id: str) -> None:
     subject = "You're Invited!"
     link = f"{os.environ['SITE_BASE_URL']}/invite/{invite_id}"
-    body = f"Hi {name},\n\nYou're invited! Click your personal invite link to RSVP:\n{link}\n\nSee you there!"
+    body_text = f"Hi {name},\n\nYou're invited! Click your personal invite link to RSVP:\n{link}\n\nSee you there!"
+    body_html = f"""
+    <html>
+        <body>
+            <p>Hi {name},</p>
+            <p>You're invited! Click your personal invite link to RSVP:<br>
+            <a href="{link}">{link}</a></p>
+            <p>See you there!</p>
+        </body>
+    </html>
+    """
 
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = formataddr((os.environ.get('EMAIL_FROM_NAME'), os.environ['EMAIL_FROM']))
-    msg['To'] = email
+    from_email = f"{os.environ.get('EMAIL_FROM_NAME')} <{os.environ['EMAIL_FROM']}>"
 
-    with smtplib.SMTP(os.environ['EMAIL_HOST'], int(os.environ['EMAIL_PORT'])) as server:
-        server.starttls()
-        server.login(os.environ['EMAIL_USERNAME'], os.environ['EMAIL_PASSWORD'])
-        server.sendmail(msg['From'], [email], msg.as_string())
+    try:
+        ses.send_email(
+            Source=from_email,
+            Destination={"ToAddresses": [email]},
+            Message={
+                "Subject": {"Data": subject},
+                "Body": {
+                    "Text": {"Data": body_text},
+                    "Html": {"Data": body_html},
+                }
+            }
+        )
+    except ClientError as e:
+        print(f"SES error: {e.response['Error']['Message']}")
